75ce2e4ff5369699ae526b3d625a3ed037dd001c
jpratt-mru <jpratt@mtroyal.ca>
2019-04-24 09:20:37 -0600

    Nonempty barcode shows '.00'.

--- src/point/of/sale/exercise/PointOfSaleSystem.java
+++ src/point/of/sale/exercise/PointOfSaleSystem.java
@@ -2,12 +2,19 @@ package point.of.sale.exercise;
 
 class PointOfSaleSystem {
 	
+	String lastBarcodeRead = "";
+	
 	public void onBarcode(String barcode) {
-		
+		lastBarcodeRead = barcode;
 	}
 	
-	public String lastResult() {
-		return "unknown item";
+	public String isDisplaying() {
+		if (lastBarcodeRead.isEmpty()) {
+			return "unknown item";
+		} else {
+			return "$1.00";
+		}
+		
 	}
 	
 }
--- tests/point/of/sale/exercise/PointOfSaleSystemTests.java
+++ tests/point/of/sale/exercise/PointOfSaleSystemTests.java
@@ -10,10 +10,19 @@ class PointOfSaleSystemTests {
 
 	@Test
 	@DisplayName("an empty barcode returns 'unknown item'")
-	void test() {
+	void test01() {
 		PointOfSaleSystem poss = new PointOfSaleSystem();
 		poss.onBarcode("");
-		assertThat(poss.lastResult()).isEqualTo("unknown item");
+		assertThat(poss.isDisplaying()).isEqualTo("unknown item");
+	}
+	
+	@Test
+	@DisplayName("a barcoded item worth 1 dollar returns '$1.00'")
+	void test02() {
+		PointOfSaleSystem poss = new PointOfSaleSystem();
+		poss.onBarcode("58");
+		assertThat(poss.isDisplaying()).isEqualTo("$1.00");
 	}
 
 }
+
