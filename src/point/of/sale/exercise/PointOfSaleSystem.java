3162d55e16d52cf3574e96fac2c03aedcc3bb041
jpratt-mru <jpratt@mtroyal.ca>
2019-04-24 10:20:12 -0600

    Introduce BarcodeToCostMap.

--- src/point/of/sale/exercise/PointOfSaleSystem.java
+++ src/point/of/sale/exercise/PointOfSaleSystem.java
@@ -17,12 +17,19 @@ class PointOfSaleSystem {
 	}
 
 	private String costForItemWithBarcode(String barcode) {
+		BarcodeToCostMap barcodeToCostMap = new BarcodeToCostMap();
+		return barcodeToCostMap.costFor(barcode);
+	}
+
+
+}
+
+class BarcodeToCostMap {
+	public String costFor(String barcode) {
 		if (barcode.equals("58")) {
 			return "$1.00";
 		} else {
 			return "$2.00";
 		}
 	}
-
-
 }
